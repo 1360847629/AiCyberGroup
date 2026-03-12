aaa191bea0104b100b635d9205bfa0d650d2e9cc
Karla-Ju <karlajuau@gmail.com>
2026-03-11 21:57:34 -0400

    Added Artificial Intelligence Section To Home Page

--- src/main/webapp/app/modules/home/home.tsx
+++ src/main/webapp/app/modules/home/home.tsx
@@ -53,6 +53,27 @@ export const Home = () => {
         </Container>
       </section>
 
+      {/* AI Section */}
+      <section className="AI-section">
+        <Container>
+          <div className="AI-card">
+            <h2 className="AI-title">
+              <Translate contentKey="home.artificial-intelligence">Artificial Intelligence</Translate>
+            </h2>
+            <p className="AI-description">
+              <Translate contentKey="home.AI-description">
+                Artificial Intelligence (AI) is revolutionizing the cybersecurity landscape by providing advanced tools and techniques to
+                detect, prevent, and respond to cyber threats. AI-powered systems can analyze vast amounts of data in real-time, identify
+                patterns, and predict potential attacks, enabling organizations to stay one step ahead of threat actors. At Cyber AI Group,
+                we leverage the power of AI to enhance cybersecurity defenses and protect digital assets. Our AI Cybersecurity soltuions are
+                designed to provide comprehensive protection against the evolving threat landscape and capitalize on AI initiative to
+                enhance cybersecurity capabiltiies.
+              </Translate>
+            </p>
+          </div>
+        </Container>
+      </section>
+
       {/* Main Content Section */}
       <section className="main-content">
         <Container>
